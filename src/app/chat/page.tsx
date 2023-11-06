d3f94bfae9a110f4883d0cceeaeaed071ec7fce5
Gagan Arora <arorag2577@gmail.com>
2023-11-06 22:51:41 +0530

    toploader

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,3 +1,46 @@
+"use client";
+import { UserButton } from "@clerk/nextjs";
+import { StreamChat } from "stream-chat";
+import {
+  Channel,
+  Chat,
+  Window,
+  ChannelHeader,
+  MessageList,
+  MessageInput,
+  Thread,
+} from "stream-chat-react";
+const userId = "user_2WQFuinAhyqddTQRROxRLF5rkyN";
+
+const chatClient = StreamChat.getInstance(process.env.NEXT_PUBLIC_STREAM_KEY!);
+
+chatClient.connectUser(
+  {
+    id: userId,
+    name: "Gagan Arora",
+  },
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoidXNlcl8yV1FGdWluQWh5cWRkVFFSUk94UkxGNXJreU4ifQ.MFZGhXXFvePT-yTVlEir9JIKlv2StFWBv-GlhmBVA0w"
+);
+
+const channel = chatClient.channel("messaging", "channel_1", {
+  name: "channel #1",
+  members: [userId],
+});
+
 export default function ChatPage() {
-  return <div>/Chat page</div>;
+  return (
+    <div>
+      <UserButton afterSignOutUrl="/" />
+      <Chat client={chatClient}>
+        <Channel channel={channel}>
+          <Window>
+            <ChannelHeader />
+            <MessageList />
+            <MessageInput />
+          </Window>
+          <Thread />
+        </Channel>
+      </Chat>
+    </div>
+  );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+import NextTopLoader from "nextjs-toploader";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
+          <NextTopLoader />
           <main>{children}</main>
         </body>
       </html>
